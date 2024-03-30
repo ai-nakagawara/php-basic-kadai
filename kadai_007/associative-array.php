76c3da1f055d063ca1929cfef92ba6a1ff705bd7
ai-nakagawara <163296333+ai-nakagawara@users.noreply.github.com>
2024-03-31 00:26:57 +0900

    kadai_007提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset = "utf-8">
+    <title>PHP基礎編_課題7</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $vegetable = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+        // 値出力
+        print_r($vegetable);
+        ?>
+    </p>
+</body>
+</html>
